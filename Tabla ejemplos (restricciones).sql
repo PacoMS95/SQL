da92e26c49bd8ca25c7ec42dd242e7cdc0f4c6d0
PacoMS95 <paco280295@yahoo.com>
2016-11-14 14:37:54 +0100

    Tabla de restricciones comenzada
    Esto es tan solo el comienzo...

---
+++ Tabla ejemplos (restricciones).sql
@@ -0,0 +1,9 @@
+CREATE TABLE Ejemplos
+GO
+USE Ejemplos
+GO
+
+CREATE TABLE DatosRestrictivos (
+ID smallint IDENTITY (1,2) NOT NULL -- falta clave primaria http://www.w3schools.com/sql/sql_primarykey.asp
+,
+)

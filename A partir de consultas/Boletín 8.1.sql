98a3912fa6d4f73b0a7a0dcce80d280faa9d91c0
Francisco Mendoza Sánchez <paco280295@yahoo.com>
2017-01-18 18:32:03 +0100

    un poquillo del 8.1

---
+++ A partir de consultas/Boletín 8.1.sql
@@ -0,0 +1,57 @@
+--Boletin 8.1
+
+--Escribe el código SQL necesario para realizar las siguientes operaciones sobre la base de datos "NorthWind”
+--Consultas sobre una sola Tabla
+use Northwind
+go
+--1. Nombre del país y número de clientes de cada país, ordenados alfabéticamente por el nombre del país.
+select country, count (CustomerID) as numeroclientes from Customers group by Country order By Country asc
+
+--2. ID de producto y número de unidades vendidas de cada producto
+select ProductID, count(Quantity) as cantidad from [Order Details] group by ProductID
+
+
+--3. ID del cliente y número de pedidos que nos ha hecho.
+
+select CustomerID, count(OrderID) as numeroDePedidos from Orders group by CustomerID
+
+
+--4. ID del cliente, año y número de pedidos que nos ha hecho cada año.
+
+
+
+
+--5. ID del producto, precio unitario y total facturado de ese producto, ordenado por cantidad facturada de mayor a menor. Si hay varios precios unitarios para el mismo producto tomaremos el mayor.
+
+
+
+
+
+--6. ID del proveedor e importe total del stock acumulado de productos correspondientes a ese proveedor.
+
+
+
+
+
+--7. Número de pedidos registrados mes a mes de cada año.
+
+
+
+
+
+--8. Año y tiempo medio transcurrido entre la fecha de cada pedido (OrderDate) y la fecha en la que lo hemos enviado (ShipDate), en días para cada año.
+
+
+
+
+
+--9. ID del distribuidor y número de pedidos enviados a través de ese distribuidor.
+
+
+
+
+
+--10. ID de cada proveedor y número de productos distintos que nos suministra.
+
+
+

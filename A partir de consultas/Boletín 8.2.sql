8f38ad78895c64ba35805e8dd280581cbd860c43
PacoMS95 <paco280295@yahoo.com>
2017-01-19 13:58:34 +0100

    8.2 creado

---
+++ A partir de consultas/Boletín 8.2.sql
@@ -0,0 +1,34 @@
+--Boletin 8.2
+--Consultas sobre una sola Tabla con agregados
+
+--Escribe el código SQL necesario para realizar las siguientes operaciones
+--sobre la base de datos "pubs”
+
+
+--1. Numero de libros que tratan de cada tema
+
+
+
+--2. Número de autores diferentes en cada ciudad y estado
+
+
+
+--3. Nombre, apellidos, nivel y antigüedad en la empresa de los empleados con un nivel entre 100 y 150.
+
+
+
+--4. Número de editoriales en cada país. Incluye el país.
+
+
+
+--5. Número de unidades vendidas de cada libro en cada año (title_id, unidades y año).
+
+
+
+--6. Número de autores que han escrito cada libro (title_id y numero de autores).
+
+
+
+--7. ID, Titulo, tipo y precio de los libros cuyo adelanto inicial (advance) tenga un valor superior a $7.000, ordenado por tipo y título
+
+

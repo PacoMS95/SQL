55fc3a72e701115f79d530b02d9ef17c9643a0df
Francisco Mendoza Sánchez <paco280295@yahoo.com>
2017-02-21 10:16:07 +0100

    algo más

--- A partir de consultas/Boletín 9.1.sql
+++ A partir de consultas/Boletín 9.1.sql
@@ -30,19 +30,26 @@ select count(ProductID) as [numero de productos], CategoryName from Products as 
 
 select CompanyName as [nombre de la compañía] from Suppliers as S inner join Products as P on S.SupplierID = P.SupplierID where ProductName in ('Tofu', 'Queso Cabrales')
 
-
 --    Empleados (ID, nombre, apellidos y teléfono) que han vendido algo a Bon app' o Meter Franken. -- ESTE SE HACE CONCATENANDO INNER JOINS. PUEDES HACERLO DE ARRIBA ABAJO O AL REVÉS, YA DEPENDE DE COMO TE GUSTE MÁS
 
 select E.EmployeeID, FirstName, LastName, HomePhone from Employees as E inner join Orders as O on E.EmployeeID = O.EmployeeID inner join Customers as C on O.CustomerID = C.CustomerID where CompanyName like 'bon%' OR CompanyName like 'Meter Franken'
 
 -- lo suyo es que uses distinct después del select, porque si te das cuentas se repiten los empleados. No está del todo mal, pero tú sabes.
 
---    Empleados (ID, nombre, apellidos, mes y día de su cumpleaños) que no han vendido nunca nada a ningún cliente de Francia. *
+--    Empleados (ID, nombre, apellidos, mes y día de su cumpleaños) que no han vendido nunca nada a ningún cliente de Francia. (NO HACE FALTA SUBCONSULTAS AUNQUE EN EL ENUNCIADO ORIGINAL SÍ LA PEDÍAN)
 
-select * from Categories
---    Total de ventas en US$ de productos de cada categoría (nombre de la categoría).
 
---    Total de ventas en US$ de cada empleado cada año (nombre, apellidos, dirección).
+
+select CustomerID from Customers where Country in ('France') except select EmployeeID,LastName,FirstName, MONTH(BirthDate) as mesDeNacimiento, day(BirthDate) from Employees
+
+
+--    Total de dinero en ventas de productos de cada categoría (nombre de la categoría).
+
+select (UnitPrice * UnitsOnOrder) as [Total recaudado con productos], CategoryName from Products as P inner join Categories as C on P.CategoryID = C.CategoryID
+
+--    Total de dinero en ventas de cada empleado cada año (nombre, apellidos, dirección).
+
+
 
 --    Ventas de cada producto en el año 97. Nombre del producto y unidades.
 
@@ -51,6 +58,5 @@ select * from Categories
 --    Empleados (nombre y apellidos) que trabajan a las órdenes de Andrew Fuller.
 
 --    Número de subordinados que tiene cada empleado, incluyendo los que no tienen ninguno. Nombre, apellidos, ID.
-
 
 --* Se necesitan subconsultas

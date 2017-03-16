3f86c76719af767c1fbc908d6282e04acb867470
Francisco Mendoza Sánchez <paco280295@yahoo.com>
2017-03-16 12:45:55 +0100

    Volvemos a GitHub

---
+++ A partir de consultas/Boletín 11.sql
@@ -0,0 +1,24 @@
+--Boletin 11.0
+
+--Sobre la base de datos LeoMetro
+use LeoMetro
+go
+
+--    Crea una función inline que nos devuelva el número de estaciones que ha recorrido cada tren en un determinado periodo de tiempo.
+-- 	  El principio y el fin de ese periodo se pasarán como parámetros
+
+--select original:
+
+select * from LM_Recorridos
+
+-- funcion:
+
+CREATE FUNCTION numeroEstacionesEnPeriodoDeTiempo ()
+RETURNS TABLE AS
+RETURN (SELECT )
+
+--    Crea una función inline que nos devuelva el número de veces que cada usuario ha entrado en el metro en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+--    Crea una función inline a la que pasemos la matrícula de un tren y una fecha de inicio y fin y nos devuelva una tabla con el número de veces que ese tren ha estado en cada estación, además del ID, nombre y dirección de la estación
+--    Crea una función inline que nos diga el número de personas que han pasado por una estacion en un periodo de tiempo. Se considera que alguien ha pasado por una estación si ha entrado o salido del metro por ella. El principio y el fin de ese periodo se pasarán como parámetros
+--	  Crea una función inline que nos devuelva los kilómetros que ha recorrido cada tren en un periodo de tiempo.El principio y el fin de ese periodo se pasarán como parámetros
+

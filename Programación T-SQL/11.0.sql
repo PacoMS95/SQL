66ae66a89f1f38ff5e3f478e7ac8a6fbd7d121f6
PacoMS95 <paco280295@yahoo.com>
2017-05-21 21:43:57 +0200

    No he avanzado na en el finde

---
+++ Programación T-SQL/11.0.sql
@@ -0,0 +1,19 @@
+--Boletin 11.0
+
+--Sobre la base de datos LeoMetro
+use LeoMetroV2
+go
+
+select * from LM_Estaciones
+--    Crea una función inline que nos devuelva el número de estaciones
+ --que ha recorrido cada tren en un determinado periodo de tiempo.
+ --El principio y el fin de ese periodo se pasarán como parámetro
+
+
+
+--    Crea una función inline que nos devuelva el número de veces que cada usuario ha entrado en el metro en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+--    Crea una función inline a la que pasemos la matrícula de un tren y una fecha de inicio y fin y nos devuelva una tabla con el número de veces que ese tren ha estado en cada estación, además del ID, nombre y dirección de la estación
+--    Crea una función inline que nos diga el número de personas que han pasado por una estacion en un periodo de tiempo. Se considera que alguien ha pasado por una estación si ha entrado o salido del metro por ella. El principio y el fin de ese periodo se pasarán como parámetros
+--    Crea una función inline que nos devuelva los kilómetros que ha recorrido cada tren en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+--    Crea una función inline que nos devuelva el número de trenes que ha circulado por cada línea en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros. Se devolverá el ID, denominación y color de la línea
+--    Crea una función inline que nos devuelva el tiempo total que cada usuario ha pasado en el metro en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros. Se devolverá ID, nombre y apellidos del pasajero. El tiempo se expresará en horas y minutos.
